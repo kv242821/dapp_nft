e5932b25514d571a3cdc438e25f54f776ca7d5d1
khue <kv242821@gmail.com>
2023-06-12 16:18:21 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto py-5">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">...</button>
+    </div>
+  );
+}
+
+export default App;
